42abf7ce52a324e0eb4027b4472a9070dd5a3e23
Rohan Rathee <rohanrathee4185@gmail.com>
2026-08-28 17:19:38 +0530

    feat: added permanent redirect from /admin to /dashboard for backwards compatibility and ease of access

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
